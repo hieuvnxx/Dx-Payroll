64e1aaf2c136d0423a058c7242f540494e168446
hieuvnxx <hieuvnxx@gmail.com>
2023-09-11 14:37:24 +0700

    update date migration zoho record value

--- src/database/migrations/2023_06_01_000004_create_zoho_record_values_table.php
+++ src/database/migrations/2023_06_01_000004_create_zoho_record_values_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             // $table->foreignId('field_id')->constrained(DxServiceProvider::DX_PREFIX_TABLE.'zoho_record_fields');
             $table->unsignedBigInteger('row_id')->nullable()->default(0);
             $table->text('value')->nullable();
-            $table->timestamp('date')->nullable();
+            $table->date('date')->nullable();
             $table->timestamp('date_time')->nullable();
         });
     }
